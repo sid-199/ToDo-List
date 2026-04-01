7a8a74ecb89b937ac721c9942e2d8bb5134bbb49
sid-199 <siddheshyadav19@gmail.com>
2026-04-02 03:30:26 +0530

    Added view file and Updated old files

--- todoapp.py
+++ todoapp.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
+import sqlite3
 
 app=Flask(__name__)
 
@@ -6,17 +7,55 @@ app=Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_task():
+    if request.method == 'POST':
+        task = request.form['task']   # get input from form
+
+        conn = sqlite3.connect('tasks.db')
+        cur = conn.cursor()
+
+        cur.execute("INSERT INTO tasks (task) VALUES (?)", (task,))
+
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('view_task'))  # go to view page
+
     return render_template('add.html')
 
 @app.route('/view')
 def view_task():
-    return "view working properly"
+    conn = sqlite3.connect('tasks.db')
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM tasks")
+    tasks = cur.fetchall()
+
+    conn.close()
+
+    return render_template('view.html', tasks=tasks)
 
 @app.route('/delete')
 def del_task():
     return "del working properly"
 
+def init_db():
+    conn = sqlite3.connect('tasks.db')   # create DB file
+    cur = conn.cursor()
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY,
+            task TEXT
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+
+
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
